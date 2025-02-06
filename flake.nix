49e8aa554cbbd21b10115faf8d09962449f25c92
dmac <DaraMac@users.noreply.github.com>
2025-02-06 15:25:52 +0000

    Adjust spacing, refactor, and add automatic gc and store optimise

--- flake.nix
+++ flake.nix
@@ -1,39 +1,48 @@
 {
-  description = "nix-darwin system flake";
+    description = "nix-darwin system flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile
-      environment.systemPackages =
-        [
-        ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        nix-darwin.url = "github:LnL7/nix-darwin/master";
+        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#Daras-MacBook-Air
-    darwinConfigurations."Daras-MacBook-Air" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-  };
+
+    outputs = inputs@{ self, nix-darwin, nixpkgs }:
+        let
+            configuration = { pkgs, ... }: {
+                # List packages installed in system profile
+                environment.systemPackages =
+                    [
+                    ];
+
+                nix = {
+                    settings = {
+                        # Necessary for using flakes on this system.
+                        experimental-features = "nix-command flakes";
+                        auto-optimise-store = true;
+                    };
+                    gc.automatic = true;
+                };
+
+                system = {
+                    # Set Git commit hash for darwin-version.
+                    configurationRevision = self.rev or self.dirtyRev or null;
+
+                    # Used for backwards compatibility, please read the changelog before changing.
+                    # $ darwin-rebuild changelog
+                    stateVersion = 6;
+                };
+
+
+                # The platform the configuration will be used on.
+                nixpkgs.hostPlatform = "aarch64-darwin";
+            };
+        in
+            {
+            # Build darwin flake using:
+            # $ darwin-rebuild build --flake .#Daras-MacBook-Air
+            darwinConfigurations."Daras-MacBook-Air" = nix-darwin.lib.darwinSystem {
+                modules = [ configuration ];
+            };
+        };
 }
