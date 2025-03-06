52553d44ee0d59d18d02c263d41027654fa1e141
dmac <DaraMac@users.noreply.github.com>
2025-03-06 12:34:20 +0000

    Add tmux home-manager

--- flake.nix
+++ flake.nix
@@ -158,6 +158,22 @@
                                             symlink-arrow = "→";
                                         };
                                     };
+
+                                    tmux = {
+                                        enable = true;
+                                        keyMode = "vi";
+                                        mouse = true;
+                                        terminal = "screen-256color"; # this was needed on Mac, maybe not on linux
+                                        extraConfig = ''
+                                            set -g status off
+                                            bind -Tcopy-mode MouseDragEnd1Pane send -X copy-selection
+
+                                            # To make new splits open in the same directory as current
+                                            # https://unix.stackexchange.com/a/109255
+                                            bind  %  split-window -h -c "#{pane_current_path}"
+                                            bind '"' split-window -v -c "#{pane_current_path}"
+                                        '';
+                                    };
                                 };
                             };
                         };
