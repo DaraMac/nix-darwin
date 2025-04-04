9efb71cef4558040891c99ae877da42203872d88
dmac <DaraMac@users.noreply.github.com>
2025-04-04 08:38:18 +0100

    Update flake.lock, comment out git branch that was referenced

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
         };
 
         # Homebrew installation
-        # nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-        nix-homebrew.url = "git+https://github.com/zhaofengli/nix-homebrew?ref=refs/pull/71/merge";
+        nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+        # nix-homebrew.url = "git+https://github.com/zhaofengli/nix-homebrew?ref=refs/pull/71/merge";
 
         # Optional: Declarative tap management
         homebrew-core = {
